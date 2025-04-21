3bb41cb615d3b29257cd36489da451ad4d8e5642
sundarshahi <shahithakurisundar@gmail.com>
2025-04-22 00:08:07 +0545

    chore: fix lint

--- packages/json/src/index.ts
+++ packages/json/src/index.ts
@@ -1,8 +1,8 @@
-import { promises as fs } from "fs";
-import { setWith } from "lodash";
-import * as fsPath from "path";
-import { Observable } from "rxjs";
-import { mergeMap, reduce } from "rxjs/operators";
+import { promises as fs } from 'fs';
+import { setWith } from 'lodash';
+import * as fsPath from 'path';
+import { Observable } from 'rxjs';
+import { mergeMap, reduce } from 'rxjs/operators';
 
 export type TranslationJsonEntry = {
   path: string | string[];
@@ -14,36 +14,34 @@ export type JSONFileSummary = {
   filePath: string;
 };
 
-export const toJSON =
-  (filePath: string) =>
+export const toJSON = (filePath: string) =>
   // eslint-disable-next-line implicit-arrow-linebreak
-  (entries$: Observable<TranslationJsonEntry>): Observable<JSONFileSummary> =>
-    entries$.pipe(
-      reduce(
-        (accumulator, entry) => {
-          if (Array.isArray(entry.path)) {
-            setWith(accumulator.data, entry.path, entry.value, Object);
-          } else {
-            accumulator.data[entry.path] = entry.value;
-          }
-          accumulator.entryCount++;
-
-          return accumulator;
-        },
-        { data: {} as Record<string, string>, entryCount: 0 }
-      ),
-      mergeMap(async ({ data, entryCount }) => {
-        await fs.mkdir(fsPath.dirname(filePath), { recursive: true });
-        const fileHandle = await fs.open(filePath, "w");
-        try {
-          await fileHandle.writeFile(JSON.stringify(data, null, 2));
-        } finally {
-          await fileHandle.close();
+  (entries$: Observable<TranslationJsonEntry>): Observable<JSONFileSummary> => entries$.pipe(
+    reduce(
+      (accumulator, entry) => {
+        if (Array.isArray(entry.path)) {
+          setWith(accumulator.data, entry.path, entry.value, Object);
+        } else {
+          accumulator.data[entry.path] = entry.value;
         }
+        accumulator.entryCount++;
 
-        return {
-          filePath,
-          entryCount,
-        };
-      })
-    );
+        return accumulator;
+      },
+      { data: {} as Record<string, string>, entryCount: 0 },
+    ),
+    mergeMap(async ({ data, entryCount }) => {
+      await fs.mkdir(fsPath.dirname(filePath), { recursive: true });
+      const fileHandle = await fs.open(filePath, 'w');
+      try {
+        await fileHandle.writeFile(JSON.stringify(data, null, 2));
+      } finally {
+        await fileHandle.close();
+      }
+
+      return {
+        filePath,
+        entryCount,
+      };
+    }),
+  );
